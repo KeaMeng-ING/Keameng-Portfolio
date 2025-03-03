5c59fd366c62562cd4f40e093bee56afaf129d73
KeaMeng <2023484ing@aupp.edu.kh>
2025-03-03 16:35:09 +0700

    analytics config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
 import "./main.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+// In your main app file (e.g., src/index.js or src/main.jsx)
+import { Analytics } from "@vercel/analytics/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+    <Analytics />
+  </React.StrictMode>
 );
